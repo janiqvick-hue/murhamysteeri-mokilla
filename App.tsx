1e865b61678b46be976e1368397d1a36962a76ec
janiqvick-hue <jani.qvick@gmail.com>
2026-06-08 15:05:52 +0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,15 +1,3 @@
 export default function App() {
-  return (
-    <div
-      style={{
-        background: "white",
-        color: "black",
-        minHeight: "100vh",
-        padding: "40px"
-      }}
-    >
-      <h1>MURHAMYSTEERI TESTI</h1>
-      <p>Jos näet tämän, App.tsx toimii.</p>
-    </div>
-  );
+  return <h1>APP TOIMII</h1>;
 }
