54f55862c386341962e5487ad27e139382d9f581
janiqvick-hue <jani.qvick@gmail.com>
2026-06-14 18:22:59 +0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import LobbyScreen from "./LobbyScreen";
 import RoleRevealScreen from "./RoleRevealScreen";
 import MapScreen from "./MapScreen";
